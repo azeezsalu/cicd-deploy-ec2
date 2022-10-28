1f2537c6fcb5926894963e3ea0f371e17183e270
HassanSesay <hsesay@tecsecops.com>
2022-10-28 19:44:05 -0400

    update SG

--- ec2.tf
+++ ec2.tf
@@ -53,6 +53,15 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+ingress {
+    description = "https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description = "ssh access"
     from_port   = 22
